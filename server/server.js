43c9822ca0899f6f5fb2c5cba0d8a8dd23cabc15
AmberFoxTerry <amberfoxterry@gmail.com>
2026-09-15 12:31:23 +0200

    Implement WebSocket server in server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,39 @@
-const express = require("express");
-
-const app = express();
+const http = require("http");
+const { WebSocketServer } = require("ws");
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-    res.send("FoxBet server is alive 🦊");
+const server = http.createServer();
+
+const wss = new WebSocketServer({
+    server
 });
 
-app.listen(PORT, "0.0.0.0", () => {
+wss.on("connection", (socket) => {
+
+    console.log("Player connected!");
+
+    socket.send(JSON.stringify({
+        type: "welcome",
+        message: "Connected to FoxBet server 🦊"
+    }));
+
+    socket.on("message", (data) => {
+
+        console.log("Received:", data.toString());
+
+    });
+
+    socket.on("close", () => {
+
+        console.log("Player disconnected.");
+
+    });
+
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+
     console.log(`FoxBet server running on port ${PORT}`);
+
 });
